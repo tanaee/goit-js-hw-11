9d116f5d47342884984de9c7fde479909dde3e40
tanaee <tanaaeeee@gmail.com>
2025-06-15 17:03:35 +0300

    added params

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 const API_KEY = '50828766-d4c68cffebe8589d5ede8282a';
 const BASE_URL = 'https://pixabay.com/api/';
 
-getImagesByQuery();
-
 export function getImagesByQuery(query) {
   const params = {
     key: API_KEY,
     q: query,
     per_page: 9,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
   };
   return axios.get(BASE_URL, { params }).then(response => response.data);
 }
